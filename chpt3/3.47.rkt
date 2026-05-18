706e5f25c0cbb49e11a43ac76bd378b6e645be2c
Vlad Skorinov <skorinov41@gmail.com>
2026-05-18 10:01:13 +0100

    finish 3.47

--- chpt3/3.47.rkt
+++ chpt3/3.47.rkt
@@ -24,32 +24,55 @@
 ; part a
 
 (define (make-semaphore-from-mutex n)
-  (let ((issued-mutex-count 0))
+  (let ((count-mutex (make-mutex))
+        (count 0))
     (define (acquire-semaphore)
-      (if (< issued-mutex-count n)
-          (let ((process-mutex (make-mutex)))
-            (process-mutex 'acquire)
-            (set! issued-mutex-count
-                  (+ issued-mutex-count 1))
-            process-mutex)
-          (acquire-semaphore)))
+      (count-mutex 'acquire)
+      (if (< count n)
+          (begin (set! count (+ count 1))
+                 (count-mutex 'release))
+          (begin (count-mutex 'release)
+                 (acquire-semaphore))))
 
-    (define (release-semaphore mutex)
-      (mutex 'release)
-      (set! issued-mutex-count
-            (- issued-mutex-count 1)))
+    (define (release-semaphore)
+      (count-mutex 'acquire)
+      (set! count (- count 1))
+      (count-mutex 'release))
     
     (define (semaphore m)
-      (cond ((eq? m 'acquire) acquire-semaphore)
-            ((eq? m 'release) release-semaphore)))
+      (cond ((eq? m 'acquire) (acquire-semaphore))
+            ((eq? m 'release) (release-semaphore))
+            (else (error "Unknown action -- SEMAPHORE" m))))
 
     semaphore))
 
 ; part b
 
-;(define (make-semaphore n)
-;  ())
+(define (make-semaphore n)
+  (let ((count 0)
+        (cell (list false)))
+    (define (acquire-semaphore)
+      (if (test-and-set! cell)
+          (acquire-semaphore)
+          (if (< count n)
+              (begin (set! count (+ count 1))
+                     (clear! cell))
+              (begin (clear! cell)
+                     (acquire-semaphore)))))
+
+    (define (release-semaphore)
+      (if (test-and-set! cell)
+          (release-semaphore)
+          (begin (set! count (- count 1))
+                 (clear! cell))))
+  
+    (define (semaphore m)
+      (cond ((eq? m 'acquire) (acquire-semaphore))
+            ((eq? m 'release) (release-semaphore))
+            (else (error "Unknown action -- SEMAPHORE" m))))
+
+    semaphore))
 
 ; 1. A semaphore can be acquired or released
 ; 2. A semaphore of size n can be acquired n times
-; 3. A semaphore shouldn't need to know anything about its mutexes
+; 3. A semaphore cannot be acquired/released by multiple consumers at once
